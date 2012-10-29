337b65127ca962615f6a887266e3dddf667e3e55
napisani <napisani@bt>
2012-10-29 16:15:56 -0400

    additional fixes

---
+++ src/com/CoverITLive/gui/MessageListener.java
@@ -0,0 +1,65 @@
+package com.CoverITLive.gui;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import com.CoverITLive.businessobjects.JsonChatObject;
+
+public class MessageListener implements PropertyChangeListener
+{
+	private SelectorPanel selectorPanel;
+	private JFrame jFrame;
+	
+	public MessageListener(JFrame jFrame,SelectorPanel selectorPanel)
+	{
+		this.selectorPanel = selectorPanel;
+		this.jFrame = jFrame;
+	}	
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent pcEvent) // a new message was received
+	{
+		if(pcEvent.getPropertyName() != null && pcEvent.getPropertyName().equals("MESSAGE"))
+		{	
+			// A new message came in - handle it!
+			JsonChatObject jChatObj = (JsonChatObject) pcEvent.getNewValue();
+			
+			String strMessage =  jChatObj.getUsername() + 
+								 " ---> " + 
+								 jChatObj.getRecipient() + " : " +
+								 jChatObj.getMessage();
+			
+			ClientPanel cPanel;
+			
+			//if the message was sent to everyone
+			if("ALL".equals(jChatObj.getRecipient()) == true)
+			{
+				cPanel = selectorPanel.getRecipientToPanel().get("ALL"); //use the ALL panel
+			}
+			else
+			{
+				cPanel = selectorPanel.getRecipientToPanel().get(jChatObj.getUsername()); //get panel by user's name
+			}
+			
+			if(cPanel != null) // as long a panel exists to add this message to
+			{
+				cPanel.appendMessageLog(strMessage);
+			}
+			jFrame.repaint();
+			
+			
+		}
+		else if(pcEvent.getPropertyName() != null && pcEvent.getPropertyName().equals("CONNECTED_USERS"))
+		{
+			ArrayList<String> alConnectedUsers = (ArrayList<String>) pcEvent.getNewValue();
+			
+			// explicitly add the ALL recipient to all 
+			alConnectedUsers.add("ALL");
+			selectorPanel.setRecipientList(alConnectedUsers);
+			jFrame.repaint();
+		}
+	}
+}
---
+++ src/com/CoverITLive/gui/SelectorPanel.java
@@ -0,0 +1,183 @@
+package com.CoverITLive.gui;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import com.CoverITLive.client.ChatClientReader;
+import com.CoverITLive.client.ChatClientWriter;
+
+public class SelectorPanel extends JPanel implements ListSelectionListener
+{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4698585594919292136L;
+	private JList jlRecipients;
+	private DefaultListModel recipListModel;
+	private HashMap<String, ClientPanel> hmRecipientToPanel;
+	private ChatClientWriter chatWriter;
+	private Box boxRightCurrent;
+	private String strCurrentName;
+	
+	public SelectorPanel(String strUsername, ChatClientWriter chatWriter) 
+	{
+		setLayout(new BorderLayout());
+		
+		this.chatWriter = chatWriter;
+		
+		//initialize recipient list and the panel map
+		hmRecipientToPanel = new HashMap<String, ClientPanel>();
+		recipListModel = new DefaultListModel();
+		recipListModel.addElement("ALL");
+		jlRecipients = new JList(recipListModel);
+		
+		hmRecipientToPanel.put("ALL", new ClientPanel("ALL", chatWriter));
+		
+		//add the list to a scrolling pane
+	    JScrollPane pane = new JScrollPane(jlRecipients);
+
+	    //  Format the list and the buttons in a vertical box
+	    Box leftBox = new Box(BoxLayout.Y_AXIS);
+	    Box rightBox = new Box(BoxLayout.Y_AXIS);
+	    
+	    // set the current right box so it can be changed later
+	    boxRightCurrent = rightBox;
+	    
+	    //add hooks for selection listener
+	    jlRecipients.addListSelectionListener(this);
+
+	    //rightBox.add(clientPanel);
+	    
+	    leftBox.add(pane);
+	    
+	    strCurrentName = "ALL";
+	    
+	    //initialize the right box to the conversation with the group - ALL
+	    rightBox.add(hmRecipientToPanel.get(strCurrentName)); 
+	    
+	    add(leftBox, BorderLayout.WEST);
+	    add(rightBox, BorderLayout.CENTER);
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent selectionEvent)
+	{
+		//as long as the user is not currently in the middle of selecting a recipient
+		if(selectionEvent.getValueIsAdjusting() == false)
+		{
+			//get the recipient name of the selected index
+			JList list = (JList) selectionEvent.getSource();
+	        Object selectionValues[] = list.getSelectedValues();
+	        
+	        if(selectionValues.length > 0) // if one or more item is selected
+	        {
+	        	//take only the first selected item and use it - ignore extras
+	        	String strRecipient = (String) selectionValues[0];
+	        	ClientPanel cPanelCurrent = hmRecipientToPanel.get(strRecipient);
+	        	
+	        	if(cPanelCurrent != null)
+	        	{
+	        		//swap out the ClientPanel for the new recipient
+	        		swapInNewPanel(cPanelCurrent, strRecipient);
+	        	}
+	        }
+		}
+	}
+	
+	private void swapInNewPanel(ClientPanel cPanel, String strNewRecipient)
+	{
+		//box it up
+    	Box rightBox = new Box(BoxLayout.Y_AXIS);
+    	rightBox.add(cPanel);
+    	remove(boxRightCurrent);
+    	add(rightBox); //add to panel
+    	boxRightCurrent = rightBox;
+    	strCurrentName = strNewRecipient; // the current recipient name
+    	revalidate(); //refresh the panel
+	}
+	
+	private void cleanUpRecipientMap(ArrayList<String> alNewNames)
+	{
+		HashMap<String, ClientPanel> hmNewRecipients = new HashMap<String, ClientPanel>();
+		for(String strName: alNewNames)
+		{
+			ClientPanel cPanel = hmRecipientToPanel.get(strName);
+			if(cPanel == null)
+			{
+				cPanel = new ClientPanel(strName, chatWriter);
+			}
+			hmNewRecipients.put(strName, cPanel);
+		}
+		hmRecipientToPanel = hmNewRecipients;
+		
+		if(alNewNames.contains(strCurrentName) == false) // if current recipient logs off
+		{
+			ClientPanel cPanel = hmRecipientToPanel.get("ALL");
+			if(cPanel != null) //switch to ALL
+			{
+				swapInNewPanel(cPanel, "ALL");
+			}
+			else // if ALL is not there (this should never happen)
+			{
+				//cycle through all of the available recipients
+				for(Map.Entry<String, ClientPanel> entry: hmRecipientToPanel.entrySet())
+				{
+					if(entry.getValue() != null) 
+					{
+						//swap in the first panel that isn't null
+						swapInNewPanel(entry.getValue(), entry.getKey());
+						break;
+					}
+				}
+			}
+			
+		}
+	}
+	
+	public void setRecipientList(ArrayList<String> alNewRecipients)
+	{	
+		
+		recipListModel.removeAllElements();
+		for(String strRecipient: alNewRecipients)
+		{
+			recipListModel.addElement(strRecipient);
+		}
+		cleanUpRecipientMap(alNewRecipients);
+		revalidate();
+	}
+	
+	public JList getJlRecipients()
+	{
+		return jlRecipients;
+	}
+
+	public void setJlRecipients(JList jlRecipients)
+	{
+		this.jlRecipients = jlRecipients;
+	}
+
+	public HashMap<String, ClientPanel> getRecipientToPanel()
+	{
+		return hmRecipientToPanel;
+	}
+
+	public void setRecipientToPanel(HashMap<String, ClientPanel> hmRecipientToPanel)
+	{
+		this.hmRecipientToPanel = hmRecipientToPanel;
+	}
+	
+	
+}
